c544c8b12f2fc14879f9caa7d660070960df068e
Kesler <kesler.m.pereira@gmail.com>
2023-03-18 16:56:12 -0300

    feat: card similar to mash

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -57,39 +57,32 @@ function ProductCard({ product, preload }: Props) {
       class="w-full group"
     >
       <a href={url} aria-label="product link">
-        <div class="relative w-full">
-          <Image
-            src={front.url!}
-            alt={front.alternateName}
-            width={200}
-            height={279}
-            class="rounded w-full group-hover:hidden"
-            preload={preload}
-            loading={preload ? "eager" : "lazy"}
-            sizes="(max-width: 640px) 50vw, 20vw"
-          />
-          <Image
-            src={back?.url ?? front.url!}
-            alt={back?.alternateName ?? front.alternateName}
-            width={200}
-            height={279}
-            class="rounded w-full hidden group-hover:block"
-            sizes="(max-width: 640px) 50vw, 20vw"
-          />
-          {seller && (
-            <div
-              class="absolute bottom-0 hidden sm:group-hover:flex flex-col gap-2 w-full p-2 bg-opacity-10"
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.2)",
-                backdropFilter: "blur(2px)",
-              }}
-            >
-              <Sizes {...product} />
-              <Button as="a" href={product.url}>Visualizar Produto</Button>
-            </div>
-          )}
+      <div class="relative w-full group">
+      <Image
+        src={front.url!}
+        alt={front.alternateName}
+        width={200}
+        height={279}
+        class="rounded w-full group-hover:hidden"
+        preload={preload}
+        loading={preload ? "eager" : "lazy"}
+        sizes="(max-width: 640px) 50vw, 20vw"
+      />
+      <Image
+        src={front.url!}
+        alt={front.alternateName}
+        width={200}
+        height={279}
+        class="rounded w-full hidden group-hover:block"
+        sizes="(max-width: 640px) 50vw, 20vw"
+      />
+      {seller && (
+        <div class="w-full bg-gray-500 relative">
+          {/* <Sizes {...product} /> */}
+          <a href={product.url} class="bg-[#24b26d] text-white px-4 py-2 absolute bottom-0 left-0 right-0 mx-auto mb-4 opacity-0 transform translate-y-full transition duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-0 text-center">ADICIONAR A SACOLA</a>
         </div>
-
+      )}
+    </div>
         <div class="flex flex-col gap-1 py-2">
           <Text
             class="overflow-hidden overflow-ellipsis whitespace-nowrap"
@@ -98,15 +91,15 @@ function ProductCard({ product, preload }: Props) {
             {name}
           </Text>
           <div class="flex items-center gap-2">
+            <Text variant="caption" tone="price">
+              {formatPrice(price, offers!.priceCurrency!)}
+            </Text>
             <Text
               class="line-through"
               variant="list-price"
               tone="subdued"
             >
               {formatPrice(listPrice, offers!.priceCurrency!)}
-            </Text>
-            <Text variant="caption" tone="price">
-              {formatPrice(price, offers!.priceCurrency!)}
             </Text>
           </div>
         </div>
